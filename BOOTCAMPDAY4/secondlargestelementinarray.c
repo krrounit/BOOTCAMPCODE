1d25a14e37fa514205ef51edd3f8d05aa8076b0c
Rounit kumar <rounit85@gmail.com>
2025-04-06 23:18:00 +0530

    secondlargestelementinarray.c

--- BOOTCAMPDAY4/secondlargestelementinarray.c
+++ BOOTCAMPDAY4/secondlargestelementinarray.c
@@ -1,28 +1,44 @@
-#include<stdio.h>
-int arraysorted(int arr, int n)
-{
-    if(n==1 || n==0)
-    {
-        return 1;
-    }
-    return arr[n-1]>= arr[n-2]
-    && arraysorted(a,n-1);
-}
-int main()
-{
-    //int arr[5],j;
-    //printf("enter elements in array=");
-   // scanf("%d",&arr[5]);
-    
-    int arr[]={20,21,24,26,32,34};
-    int n= sizeof(arr) /sizeof(arr[0]);
-    if(arraysorted(a, n))
-    {
-        printf("yes\n");
-    }
-    else{
-        printf("no\n");
-    }
-    return 0;
+#include <stdio.h>
+#include <limits.h>  
 
+int secondlargestelement(int arr[], int n) {
+    int largest = INT_MIN, second_largest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] > second_largest && arr[i] != largest) {
+            second_largest = arr[i];
+        }
+    }
+
+    return (second_largest != INT_MIN) ? second_largest : -1;
+}
+
+int main() {
+    int n;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("Array should have at least two elements.\n");
+        return 0;
+    }
+
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int second_largest = secondlargestelement(arr, n);
+
+    if (second_largest != -1) {
+        printf("Second largest element = %d\n", second_largest);
+    } else {
+        printf("No second largest element found.\n");
+    }
+
+    return 0;
 }
